f6e2861034cba65898cf1f5b1703254354cd2d20
yarocruz <yarocruz@gmail.com>
2020-05-07 13:08:52 -0400

    deleted unnecessary files for project

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
        <p className="text-6xl">Hello from React bitches!</p>
         <img className="rounded-full" src="https://randomuser.me/api/portraits/women/90.jpg" alt="employee"/>
       </header>
