7f050361dc97d68f4ce116a0d8c9ceb17b7c5813
yarocruz <yarocruz@gmail.com>
2020-05-07 14:04:52 -0400

    added header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <header>
        <p className="text-6xl">Hello from React bitches!</p>
         <img className="rounded-full" src="https://randomuser.me/api/portraits/women/90.jpg" alt="employee"/>
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import logo from "../assets/images/ucf-logo.png";
+
+function Header(props) {
+    return (
+        <div className="bg-white shadow text-gray-800">
+            <header className="container max-w-6xl px-10 py-3 mx-auto flex justify-between items-center">
+                <div className="flex items-center">
+                    <img className="mr-3" src={logo} alt="UCF School Logo"/>
+                    <h1 className="font-bold">UCF Coding Bootcamp Project</h1>
+                </div>
+                <a href="https://github.com/yarocruz/react-employee-directory" className="hover:text-yellow-500">Code</a>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
