db201c2072f4e8a300691611b18245981d06def2
yarocruz <yarocruz@gmail.com>
2020-05-07 12:11:57 -0400

    setup tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <p>Hello from React bitches!</p>
-        <img src="https://randomuser.me/api/portraits/women/90.jpg" alt="employee"/>
+       <p className="text-6xl">Hello from React bitches!</p>
+        <img className="rounded-full" src="https://randomuser.me/api/portraits/women/90.jpg" alt="employee"/>
       </header>
     </div>
   );
