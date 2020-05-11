baf3b25fa75037be9e51306f7a4b01fc20b03ef8
yarocruz <yarocruz@gmail.com>
2020-05-11 12:07:34 -0400

    made employee image a bit bigger

--- src/components/EmployeeCard.js
+++ src/components/EmployeeCard.js
@@ -4,7 +4,7 @@ import React from "react";
     return (
         <div className="md:flex bg-white shadow text-gray-800 my-4 py-4 px-10 rounded-md items-center justify-between">
             <img
-                style={{ maxWidth: "50px"}}
+                style={{ maxWidth: "60px"}}
                 className="rounded-full shadow-md"
                 src={image}
                 alt="employee"
