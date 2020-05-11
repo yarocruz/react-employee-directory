7f2783a3770514d68276c1012cf85d5800d3af52
yarocruz <yarocruz@gmail.com>
2020-05-11 12:15:11 -0400

    Updates

---
+++ precache-manifest.e7c706cf4d675172cf95da98f76ba76d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3272b6076a136156059808446a70d99d",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "ce6a45194a1f670c8fa4",
+    "url": "/react-employee-directory/static/css/main.f6a42cf4.chunk.css"
+  },
+  {
+    "revision": "8837b8f0bf35019104da",
+    "url": "/react-employee-directory/static/js/2.b15b4a0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.b15b4a0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce6a45194a1f670c8fa4",
+    "url": "/react-employee-directory/static/js/main.f611c202.chunk.js"
+  },
+  {
+    "revision": "8e632fc8d7abb2283eb9",
+    "url": "/react-employee-directory/static/js/runtime-main.c8d5e7ba.js"
+  }
+]);
