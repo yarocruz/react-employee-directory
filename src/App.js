4d8bed56afc08d8694814a13200801fc60e5c92a
yarocruz <yarocruz@gmail.com>
2020-05-14 10:30:09 -0400

    converted the App class component to a functional component with useState hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import Layout from "./components/Layout";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
@@ -6,75 +6,61 @@ import EmployeeCardList from "./components/EmployeeCardList";
 import Footer from "./components/Footer";
 import employees from "./data/employees.json";
 
-class App extends Component {
-    state = {
-        searchTerm: '',
-        sorted: false,
-        employees,
-    }
+function App() {
+    const [ searchTerm, setSearchTerm ] = useState("");
+    const [ sorted, setSorted] = useState(false);
+    const [ data, setEmployees ] = useState(employees);
 
-    handleSearchTerm = (event) => {
+    function handleSearchTerm(event)  {
         event.preventDefault();
-        this.setState({
-            searchTerm: event.target.value,
-        })
+        setSearchTerm(event.target.value)
     }
 
-    handleSortByName = (event) => {
+    function handleSortByName(event) {
         event.preventDefault();
         // sort array ascending or descending by first name
-        if (!this.state.sorted) {
-            this.setState({
-                employees: this.state.employees.sort((a, b) => (a.name > b.name) ? 1 : -1),
-                sorted: true
-            })
+        if (!sorted) {
+            setEmployees(data.sort((a, b) => (a.name > b.name) ? 1 : -1));
+            setSorted(true);
         } else {
-            this.setState({
-                employees: this.state.employees.sort((a, b) => (a.name > b.name) ? -1 : 1),
-                sorted: false
-            })
+            setEmployees(data.sort((a, b) => (a.name > b.name) ? -1 : 1));
+            setSorted(false);
         }
     }
 
-    handleSortByDept = (event) => {
+    function handleSortByDept(event) {
         event.preventDefault();
         // sort array ascending or descending by dept name
-        if (!this.state.sorted) {
-            this.setState({
-                employees: this.state.employees.sort((a, b) => (a.department > b.department) ? 1 : -1),
-                sorted: true
-            })
+        if (!sorted) {
+            setEmployees(data.sort((a, b) => (a.department > b.department) ? 1 : -1));
+            setSorted(true);
         } else {
-            this.setState({
-                employees: this.state.employees.sort((a, b) => (a.department > b.department) ? -1 : 1),
-                sorted: false
-            })
+            setEmployees(data.sort((a, b) => (a.department > b.department) ? -11 : 1));
+            setSorted(false);
         }
     }
 
-    render() {
-        // the filteredEmployees variable only stores employee names that start with with the matching string you type
-        const filteredEmployees = this.state.employees.filter(employee => employee.name.toLowerCase().startsWith(this.state.searchTerm.toLowerCase()));
-        return (
-            <div>
-                <Header/>
-                <Layout>
-                    <h1 className="title text-5xl text-gray-800 mt-16">Employee Directory</h1>
-                    <p className="mb-16 text-md">Search for an employee or sort by Name or Category.</p>
-                    {/*the handleSearchTerm method and searchTerm state get passed down to the Navigation component via props with the onSearch and searchTerm props*/}
-                    <Navigation
-                        onSearch={this.handleSearchTerm}
-                        searchTerm={this.state.searchTerm}
-                        handleSortByName={this.handleSortByName}
-                        handleSortByDept={this.handleSortByDept}
-                    />
-                    {/* the employees array gets the filteredEmployees data via the data prop */}
-                    <EmployeeCardList data={filteredEmployees}/>
-                    <Footer/>
-                </Layout>
-            </div>
-        );
-    }
+    // the filteredEmployees variable only stores employee names that start with with the matching string you type
+    const filteredEmployees = data.filter(employee => employee.name.toLowerCase().startsWith(searchTerm.toLowerCase()));
+    return (
+        <div>
+            <Header/>
+            <Layout>
+                <h1 className="title text-5xl text-gray-800 mt-16">Employee Directory</h1>
+                <p className="mb-16 text-md">Search for an employee or sort by Name or Category.</p>
+                {/*the handleSearchTerm method and searchTerm state get passed down to the Navigation component via props with the onSearch and searchTerm props*/}
+                <Navigation
+                    onSearch={handleSearchTerm}
+                    searchTerm={searchTerm}
+                    handleSortByName={handleSortByName}
+                    handleSortByDept={handleSortByDept}
+                />
+                {/* the employees array gets the filteredEmployees data via the data prop */}
+                <EmployeeCardList data={filteredEmployees}/>
+                <Footer/>
+            </Layout>
+        </div>
+    )
 }
 
 export default App;
